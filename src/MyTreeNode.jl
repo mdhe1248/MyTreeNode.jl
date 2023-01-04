fb1cd46ed2434c6c2f0c60ca3eab8f2d14b7a2fa
Donghoon Lee <mdhe1248@gmail.com>
2023-01-04 08:58:21 -0500

    bug fix: remove comma

--- src/MyTreeNode.jl
+++ src/MyTreeNode.jl
@@ -1,6 +1,6 @@
 module MyTreeNode
 
-export TreeNode, mergetree, _mergetree, mergetree!, appendtree!, IDTree,  
+export TreeNode, mergetree, _mergetree, mergetree!, appendtree!, IDTree
 include("treenode.jl")
 
 end
